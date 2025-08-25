1bdc6996f2172564d77e23b20661bb1b3335f44b
KARINA <u24263261@utp.edu.pe>
2025-08-24 23:19:53 -0500

    Update Vehiculo.java

--- POO VEHICULOS/src/main/java/com/mycompany/poo/Vehiculo.java
+++ POO VEHICULOS/src/main/java/com/mycompany/poo/Vehiculo.java
@@ -1,39 +1,85 @@
 package com.mycompany.poo;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vehiculo {
-    private String marca;
-    private String modelo;
-    private int anio;
 
-    public Vehiculo(String marca, String modelo, int anio) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.anio = anio;
+    private String codigo;
+    private String placa;
+    private String numeroSerie;
+    private int añoDeFabricacion;
+    private String color;
+    private int cantidadPuertas;
+    private double cilindrada;
+    private String dueño;
+    
+    private static final String NOMBRE_ARCHIVO = "vehiculos.txt";
+
+    public Vehiculo(String codigo, String placa, String numeroSerie, int añoDeFabricacion, String color, int cantidadPuertas, double cilindrada, String dueño) {
+        this.codigo = codigo;
+        this.placa = placa;
+        this.numeroSerie = numeroSerie;
+        this.añoDeFabricacion = añoDeFabricacion;
+        this.color = color;
+        this.cantidadPuertas = cantidadPuertas;
+        this.cilindrada = cilindrada;
+        this.dueño = dueño;
     }
 
-    public String getMarca() {
-        return marca;
+    public String getCodigo() {
+        return codigo; 
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo; 
     }
 
-    public String getModelo() {
-        return modelo;
+    public String getPlaca() {
+        return placa; 
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
     }
 
-    public int getAnio() {
-        return anio;
+    public String getNumeroSerie() {
+        return numeroSerie; 
+    }
+    public void setNumeroSerie(String numeroSerie) { 
+        this.numeroSerie = numeroSerie; 
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
+    public int getAñoDeFabricacion() {
+        return añoDeFabricacion;
+    }
+    public void setAñoDeFabricacion(int añoDeFabricacion) {
+        this.añoDeFabricacion = añoDeFabricacion; 
     }
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
+    public String getColor() { 
+        return color; 
+    }
+    public void setColor(String color) { 
+        this.color = color; 
     }
 
-    public void setAnio(int anio) {
-        if (anio > 1900 && anio <= 2025) {
-            this.anio = anio;
-        }
+    public int getCantidadPuertas() {
+        return cantidadPuertas; 
     }
-}
+    public void setCantidadPuertas(int cantidadPuertas) {
+        this.cantidadPuertas = cantidadPuertas; 
+    }
+
+    public double getCilindrada() {
+        return cilindrada; 
+    }
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada; 
+    }
+
+    public String getDueño() {
+        return dueño; 
+    }
+    public void setDueño(String dueño) {
+        this.dueño = dueño; 
+    }
