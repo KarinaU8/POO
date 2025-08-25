d0fe63502fedf5cd9422c71d3ff80879e9b2b6fd
KARINA <u24263261@utp.edu.pe>
2025-08-24 23:21:24 -0500

    Update Cliente.java

--- POO VEHICULOS/src/main/java/com/mycompany/poo/Cliente.java
+++ POO VEHICULOS/src/main/java/com/mycompany/poo/Cliente.java
@@ -1,46 +1,69 @@
 package com.mycompany.poo;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
-    private String nombre;
-    private String dni;
-    private String telefono;
-    private Vehiculo vehiculo;
+    private String codigo;
+    private String nombres;
+    private String apellidos;
+    private String direccion;
+    private String sexo;
+    private String correo;
+    private String celular;
 
-    public Cliente(String nombre, String dni, String telefono) {
-        this.nombre = nombre;
-        this.dni = dni;
-        this.telefono = telefono;
+    private static final String NOMBRE_ARCHIVO = "clientes.txt";
+
+    public Cliente(String codigo, String nombres, String apellidos, String direccion, String sexo, String correo, String celular) {
+        this.codigo = codigo;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.sexo = sexo;
+        this.correo = correo;
+        this.celular = celular;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getCodigo() {
+        return codigo; 
     }
-
-    public String getDni() {
-        return dni;
+    public void setCodigo(String codigo) {
+        this.codigo = codigo; 
     }
-
-    public String getTelefono() {
-        return telefono;
+    public String getNombres() {
+        return nombres; 
     }
-
-    public Vehiculo getVehiculo() {
-        return vehiculo;
+    public void setNombres(String nombres) {
+        this.nombres = nombres; 
     }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public String getApellidos() { 
+        return apellidos; 
     }
-
-    public void setDni(String dni) {
-        this.dni = dni;
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos; 
     }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public String getDireccion() {
+        return direccion; 
     }
-
-    public void setVehiculo(Vehiculo vehiculo) {
-        this.vehiculo = vehiculo;
+    public void setDireccion(String direccion) {
+        this.direccion = direccion; 
     }
-}
+    public String getSexo() {
+        return sexo; 
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo; 
+    }
+    public String getCorreo() {
+        return correo; 
+    }
+    public void setCorreo(String correo) { 
+        this.correo = correo; 
+    }
+    public String getCelular() {
+        return celular; 
+    }
+    public void setCelular(String celular) {
+        this.celular = celular; 
+    }
