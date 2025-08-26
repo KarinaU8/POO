508fd9a1c8b882bd3ce318327b24d69f66771a2e
KARINA <u24263261@utp.edu.pe>
2025-08-26 00:02:04 -0500

    Update Cliente.java

--- POO VEHICULOS/src/main/java/com/mycompany/poo/Cliente.java
+++ POO VEHICULOS/src/main/java/com/mycompany/poo/Cliente.java
@@ -1,8 +1,4 @@
-package com.mycompany.poo;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+package transporte_sur.clases;
 
 public class Cliente {
     private String codigo;
@@ -13,57 +9,35 @@ public class Cliente {
     private String correo;
     private String celular;
 
-    private static final String NOMBRE_ARCHIVO = "clientes.txt";
+    // Getters y Setters
+    public String getCodigo() { return codigo; }
+    public void setCodigo(String codigo) { this.codigo = codigo; }
 
-    public Cliente(String codigo, String nombres, String apellidos, String direccion, String sexo, String correo, String celular) {
-        this.codigo = codigo;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.direccion = direccion;
-        this.sexo = sexo;
-        this.correo = correo;
-        this.celular = celular;
+    public String getNombres() { return nombres; }
+    public void setNombres(String nombres) { this.nombres = nombres; }
+
+    public String getApellidos() { return apellidos; }
+    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+
+    public String getSexo() { return sexo; }
+    public void setSexo(String sexo) { this.sexo = sexo; }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+
+    public String getCelular() { return celular; }
+    public void setCelular(String celular) { this.celular = celular; }
+
+    @Override
+    public String toString() {
+        return codigo + " - " + nombres + " " + apellidos;
     }
 
-    public String getCodigo() {
-        return codigo; 
+    public String toCsvString() {
+        return codigo + ";" + nombres + ";" + apellidos + ";" + direccion + ";" + sexo + ";" + correo + ";" + celular;
     }
-    public void setCodigo(String codigo) {
-        this.codigo = codigo; 
-    }
-    public String getNombres() {
-        return nombres; 
-    }
-    public void setNombres(String nombres) {
-        this.nombres = nombres; 
-    }
-    public String getApellidos() { 
-        return apellidos; 
-    }
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos; 
-    }
-    public String getDireccion() {
-        return direccion; 
-    }
-    public void setDireccion(String direccion) {
-        this.direccion = direccion; 
-    }
-    public String getSexo() {
-        return sexo; 
-    }
-    public void setSexo(String sexo) {
-        this.sexo = sexo; 
-    }
-    public String getCorreo() {
-        return correo; 
-    }
-    public void setCorreo(String correo) { 
-        this.correo = correo; 
-    }
-    public String getCelular() {
-        return celular; 
-    }
-    public void setCelular(String celular) {
-        this.celular = celular; 
-    }
+}
+
